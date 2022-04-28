bff3153cb0583657ca8bb2210f2ef315d9b4bb2c
Yuri The Doer <eyeri91@gmail.com>
2022-04-28 17:41:35 +0400

    Display user input and operator

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const allClear = document.querySelector('.all-clear');
+const clear = document.querySelector('.clear');
+const savedInput = document.querySelector('.saved-input');
+const currentNum = document.querySelector('.current-num');
+
+let input = '';
+let savedNum = '';
 
 function add(num1, num2) {
     return num1 + num2;
@@ -30,3 +39,30 @@ function round(num) {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
+
+numbers.forEach(number =>
+    (number.addEventListener('click', () => getNewNumber(number.textContent)))
+)
+
+// How to avoid having 0 as the first number?
+function getNewNumber(number) {
+    currentNum.innerHTML += number;
+}
+
+operators.forEach(operator =>
+    (operator.addEventListener('click', () => displayNewNum(operator.textContent)))
+)
+
+function displayNewNum(operator) {
+    savedNum += currentNum.innerHTML;
+    savedNum += ` ${operator} `
+    savedInput.innerHTML += savedNum;
+
+    // Reset the current
+    resetCurrentNum();
+    savedNum = '';
+}
+
+
+resetCurrentNum = () => currentNum.innerHTML = '';
+// resetSavedNum = () => savedNum = '';
