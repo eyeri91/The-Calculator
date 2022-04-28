b78c6aaccaf7a0cce1ca5d74fb36af8aa7aefc0b
Yuri The Doer <eyeri91@gmail.com>
2022-04-28 13:44:29 +0400

    Create 4 math operator functions

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Round the long decmials 
+// Avoid 'divide by 0'
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "Infinity"
+    }
+    // return num1 / num2;
+    return round(num1 / num2);
+}
+
+function round(num) {
+    let fNum = Number((Math.abs(num) * 10000).toPrecision(15));
+    return Math.round(fNum) / 10000 * Math.sign(num);
+}
