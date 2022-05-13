595cab5e19be73c8296362efa84969f15393e42f
Yuri The Doer <eyeri91@gmail.com>
2022-05-13 15:40:31 +0400

    Make all buttons responsive to operate math expressions

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ class Calculator {
     constructor(preOperandTextElement, curOperandTextElement) {
         this.preOperandTextElement = preOperandTextElement;
         this.curOperandTextElement = curOperandTextElement;
+        this.allClear()
     }
 
     clear() {
-
+        this.currentOperand = this.currentOperand.slice(0, -1);
+        this.updateDisply()
     }
 
     allClear() {
-
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operator = undefined;
+        this.updateDisply()
     }
 
     add() {
@@ -28,13 +33,52 @@ class Calculator {
 
     }
 
-    getOperator() {
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes(".")) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
 
+    getOperator(operator) {
+        if (this.currentOperand === '') return
+        this.operator = operator;
+        this.previousOperand = `${this.currentOperand} ${operator}`
+        this.currentOperand = '';
+        // this.previousOperand = this.currentOperand + operator;
     }
 
     calculate() {
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return
 
+        switch (this.operator) {
+            case '+':
+                computation = prev + current;
+                break
+            case '-':
+                computation = prev - current;
+                break
+            case 'x':
+                computation = prev * current;
+                break
+            case '÷':
+                computation = prev / current;
+                break
+            case '%':
+                computation = prev % current;
+                break
+            default:
+                return
+
+        }
     }
+
+    updateDisply() {
+        this.curOperandTextElement.innerText = this.currentOperand;
+        this.preOperandTextElement.innerText = this.previousOperand;
+    }
+
 }
 
 
@@ -43,165 +87,34 @@ const operators = document.querySelectorAll('.operator');
 const allClear = document.querySelector('.all-clear');
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
-const decimal = document.querySelector('.decimal');
 const preOperandTextElement = document.querySelector('.previous-operand');
 const curOperandTextElement = document.querySelector('.current-operand');
 
-// let previousNum = '';
-// let nextNum = '';
-// let curOperator = '';
-// let savedNum;
-// curOperand.innerHTML = 0;
 
+const calculator = new Calculator(preOperandTextElement, curOperandTextElement)
 
-// function add(num1, num2) {
-//     return num1 + num2;
-// }
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisply()
+    })
+})
 
-// function subtract(num1, num2) {
-//     return num1 - num2;
-// }
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.getOperator(button.innerText);
+        calculator.updateDisply()
+    })
+})
 
-// function multiply(num1, num2) {
-//     return num1 * num2;
-// }
+equals.addEventListener('click', () => {
+    calculator.calculate()
+})
 
-// function divide(num1, num2) {
-//     if (num2 === 0) {
-//         alert("Infinity! Don't divide by 0")
-//         return curOperand.innerHTML = null;
-//     }
-//     return round(num1 / num2);
-// }
+allClear.addEventListener('click', () => {
+    calculator.allClear()
+})
 
-// function remainder(num1, num2) {
-//     return num1 % num2;
-// }
-
-// function round(num) {
-//     let fNum = Number((Math.abs(num) * 10000).toPrecision(15));
-//     return Math.round(fNum) / 10000 * Math.sign(num);
-// }
-
-
-// equals.addEventListener('click', () => evaluate());
-
-// function evaluate() {
-//     let previousInput = preOperand.innerHTML;
-//     let currentInput = curOperand.innerHTML;
-//     if ((previousInput === '' && currentInput === '')
-//         || (previousInput === '' && currentInput !== '')
-//         || (previousInput !== '' && currentInput === '')) return;
-
-
-//     let preInputList = preOperand.innerHTML.trim().split(' ');
-//     let result = calculateByOrder(preInputList);
-//     displayResult(result);
-// }
-
-
-// function calculateByOrder(preInputList) {
-//     let num1 = Number(preInputList[0]);
-//     let operator = preInputList[1];
-
-//     if (preInputList.length === 2) {
-//         let num2 = Number(curOperand.innerHTML);
-//         return operate(num1, operator, num2);
-
-//     } else {
-//         let num2 = Number(preInputList[2]);
-//         let previousOperation = operate(num1, operator, num2);
-//         preInputList.splice(0, 3);
-//         preInputList.unshift(previousOperation);
-//         return calculateByOrder(preInputList);
-//     }
-// }
-
-// function operate(num1, operator, num2) {
-//     if (operator === '+') {
-//         operator = add;
-//     } else if (operator === '-') {
-//         operator = subtract;
-//     } else if (operator === 'x') {
-//         operator = multiply;
-//     } else if (operator === '÷') {
-//         operator = divide;
-//     } else if (operator === '%') {
-//         operator = remainder;
-//     }
-//     return operator(num1, num2);
-// }
-
-// function displayResult(result) {
-//     preOperand.innerHTML = '';
-//     curOperand.innerHTML = result;;
-// }
-
-
-// allClear.addEventListener('click', resetAll)
-
-// function resetAll() {
-//     preOperand.innerHTML = '';
-//     curOperand.innerHTML = '0';
-//     previousNum = '';
-//     nextNum = '';
-//     curOperator = '';
-//     savedNum = '';
-// }
-
-
-// clear.addEventListener('click', clearLastInput)
-
-// function clearLastInput() {
-//     let previousInput = preOperand.innerHTML;
-//     let currentInput = curOperand.innerHTML;
-//     if (currentInput === '' && previousInput === '') return
-//     if (currentInput !== '') {
-//         nextNum = curOperand.innerHTML.slice(0, -1);
-//         curOperand.innerHTML = nextNum;
-//     }
-//     if (previousInput !== '' && currentInput === '') {
-//         previousNum = preOperand.innerHTML.slice(0, -1);
-//         preOperand.innerHTML = previousNum;
-//     }
-// }
-
-
-// decimal.addEventListener('click', addDecimal);
-
-// function addDecimal() {
-//     if (curOperand.innerHTML === '' || curOperand.innerHTML.includes('.')) return
-//     curOperand.innerHTML += '.';
-// }
-
-
-// numbers.forEach(number =>
-//     (number.addEventListener('click', () => getNewOperand(number.textContent)))
-// )
-
-// function getNewOperand(number) {
-//     let zeroRemovedNum = Number(curOperand.innerHTML + number);
-//     curOperand.innerHTML = zeroRemovedNum;
-// }
-
-
-// operators.forEach(operator =>
-//     (operator.addEventListener('click', () => addOperator(operator.textContent)))
-// )
-
-// function addOperator(operator) {
-//     let currentNum = curOperand.innerHTML;
-//     let previousInput = preOperand.innerHTML;
-//     savedNum = currentNum;
-//     curOperator = operator;
-
-//     if (currentNum === '' && previousInput === '') return alert("Enter the operand first")
-
-//     preOperand.innerHTML += ` ${Number(currentNum)} ${curOperator}`;
-
-//     resetCurrentNum();
-//     return savedNum, curOperator;
-// }
-
-// resetCurrentNum = () => curOperand.innerHTML = '';
-
+clear.addEventListener('click', () => {
+    calculator.clear()
+})
